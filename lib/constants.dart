e7f31a06ab9c8ed940a69902c11d51b3aa2234b9
sushant102004 <sushant.dhiman9812@gmail.com>
2022-09-13 23:22:14 +0530

    Initial Commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  final textColor = const Color.fromARGB(255, 49, 55, 43);
+  final primaryColor = const Color.fromARGB(255, 91, 140, 41);
+}
