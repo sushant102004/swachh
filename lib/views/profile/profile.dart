b7ae206a73cbb9335d9323f2e6effbc0f30de92c
sushant102004 <sushant.dhiman9812@gmail.com>
2022-11-08 10:55:55 +0530

    Profie Card Added

--- lib/views/profile/profile.dart
+++ lib/views/profile/profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:swachh/constants.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -8,8 +10,77 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final constants = Get.put(Constants());
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            SizedBox(
+              height: Get.height / 15,
+            ),
+            Container(
+              width: Get.width / 1.07,
+              height: Get.height / 7,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.grey.shade200,
+                  boxShadow: [
+                    BoxShadow(color: Colors.grey.shade300, blurRadius: 10)
+                  ]),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: Get.width / 20),
+                    child: Container(
+                      width: Get.width / 6,
+                      height: Get.height / 13,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.shade400,
+                          borderRadius: BorderRadius.circular(200)),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: Get.width / 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Heavenly Demon',
+                          style: TextStyle(
+                              color: constants.primaryColor,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: Get.height / 70,
+                        ),
+                        Flexible(
+                          child: Container(
+                            width: Get.width / 1.5,
+                            child: Text(
+                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                              style: TextStyle(
+                                  color: constants.textColor,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
