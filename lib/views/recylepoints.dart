85a906b7da8eded038085497ff377cab62587814
sushant102004 <sushant.dhiman9812@gmail.com>
2022-11-13 14:57:55 +0530

    Map Implementation Started

--- lib/views/recylepoints.dart
+++ lib/views/recylepoints.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+LatLng currentLocation = const LatLng(30.2753, 77.0476);
 
 class RecylePoints extends StatefulWidget {
   const RecylePoints({Key? key}) : super(key: key);
@@ -8,11 +11,30 @@ class RecylePoints extends StatefulWidget {
 }
 
 class _RecylePointsState extends State<RecylePoints> {
+  late GoogleMapController mapController;
+  Map<String, Marker> _markers = {};
+
+  addMarker(String id, LatLng location) {
+    var marker = Marker(
+        markerId: MarkerId(id),
+        position: location,
+        infoWindow: const InfoWindow(
+            title: 'Recycle Point',
+            snippet: 'Description about Recycle Point'));
+    _markers[id] = marker;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Recyle Points'),
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition:
+            CameraPosition(target: currentLocation, zoom: 14),
+        onMapCreated: ((controller) {
+          mapController = controller;
+          addMarker('First Marker', currentLocation);
+        }),
+        markers: _markers.values.toSet(),
       ),
     );
   }
