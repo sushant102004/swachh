28f9166c677c11dc67fb46e318220f3f4a011425
sushant102004 <sushant.dhiman9812@gmail.com>
2022-11-24 22:19:48 +0530

    Location Search Widget Added

--- lib/views/recylepoints.dart
+++ lib/views/recylepoints.dart
@@ -19,6 +19,8 @@ class RecylePoints extends StatefulWidget {
 }
 
 class _RecylePointsState extends State<RecylePoints> {
+  final constants = Get.put(Constants());
+
   late GoogleMapController mapController;
   Map<String, Marker> _markers = {};
 
@@ -43,21 +45,70 @@ class _RecylePointsState extends State<RecylePoints> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Obx(() => isCurrentLocationFected.value == true
-          ? GoogleMap(
-              initialCameraPosition:
-                  CameraPosition(target: currentLocation, zoom: 14),
-              onMapCreated: ((controller) {
-                mapController = controller;
-                addMarker('First Marker', currentLocation);
-              }),
-              markers: _markers.values.toSet(),
-            )
-          : Center(
-              child: CircularProgressIndicator(
-                color: Constants().primaryColor,
+        body: Stack(
+      children: [
+        Positioned.fill(
+          child: Obx(() => isCurrentLocationFected.value == true
+              ? GoogleMap(
+                  initialCameraPosition:
+                      CameraPosition(target: currentLocation, zoom: 14),
+                  onMapCreated: ((controller) {
+                    setState(() {
+                      mapController = controller;
+                      addMarker('First Marker', currentLocation);
+                    });
+                  }),
+                  markers: _markers.values.toSet(),
+                )
+              : Center(
+                  child: CircularProgressIndicator(
+                    color: Constants().primaryColor,
+                  ),
+                )),
+        ),
+        Positioned(
+            top: 70,
+            child: Padding(
+              padding: EdgeInsets.only(left: Get.width / 18),
+              child: Container(
+                width: Get.width / 1.13,
+                height: Get.height / 13.5,
+                decoration: BoxDecoration(
+                    color: constants.backgroundColor,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade300,
+                        offset: const Offset(0.0, 0.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 2.0,
+                      )
+                    ]),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: Get.width / 20),
+                      child: Text(
+                        'Search Your Location',
+                        style: TextStyle(
+                            color: Colors.grey.shade600, fontSize: 18),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: Get.width / 40),
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.search,
+                            color: Colors.grey.shade500,
+                          )),
+                    )
+                  ],
+                ),
               ),
-            )),
-    );
+            ))
+      ],
+    ));
   }
 }
